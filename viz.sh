2ba252e75f238273b94d12fc9a91875f98d6c9d4
Michael Tsukanov <tsukanovWOW@ya.ru>
2025-05-04 23:19:06 +0300

    Remove generated python script after window closes

--- viz.sh
+++ viz.sh
@@ -5,3 +5,4 @@ printf "import matplotlib.pyplot as plt\ndata = [0] * " > viz.py
 printf "plt.plot(range(len(data)), data)\nplt.title('Acquisition rate')\nplt.xlabel('Object index')\nplt.ylabel('Times an object was acquired')\nplt.show()" >> viz.py
 
 python viz.py
+rm viz.py
